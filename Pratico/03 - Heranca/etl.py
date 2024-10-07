9216a2527db1d46abce3e9f24e6245b172376884
Durso <dursoarthur@gmail.com>
2024-10-07 20:53:17 -0300

    Finalizando prática de exercícios de OOP

--- Pratico/03 - Heranca/etl.py
+++ Pratico/03 - Heranca/etl.py
@@ -1,40 +1,37 @@
+import os
 import pandas as pd
+import sqlite3
 
+# Classe 'base'
 class ETLProcess:
     def __init__(self, fonte_dados):
         self.fonte_dados = fonte_dados
 
     def extrair_dados(self):
         raise NotImplementedError("Método extrair_dados deve ser implementado nas classes filhas.")
-
+    
     def transformar_dados(self, dados):
         raise NotImplementedError("Método transformar_dados deve ser implementado nas classes filhas.")
-
+    
     def carregar_dados(self, dados_transformados):
         raise NotImplementedError("Método carregar_dados deve ser implementado nas classes filhas.")
-
+    
     def executar_etl(self):
         dados_extraidos = self.extrair_dados()
         dados_transformados = self.transformar_dados(dados_extraidos)
-        self.carregar_dados(dados_transformados)
-
-
+        return self.carregar_dados(dados_transformados)
+    
+# Classe para CSV
 class ETLCSV(ETLProcess):
     def extrair_dados(self):
         return pd.read_csv(self.fonte_dados)
-
+    
     def transformar_dados(self, dados):
-        # Exemplo simples de transformação: converter todas as letras em maiúsculas
+        #Converter todas as letras em maiusculas
         return dados.applymap(lambda x: x.upper() if isinstance(x, str) else x)
-
+    
     def carregar_dados(self, dados_transformados):
-        # Aqui você pode implementar a lógica para carregar os dados transformados para onde desejar
-        print("Dados transformados:")
-        print(dados_transformados)
-
-
-# Exemplo de uso
-if __name__ == "__main__":
-    fonte_csv = 'dados.csv'  # Substitua 'dados.csv' pelo caminho do seu arquivo CSV
-    etl_csv = ETLCSV(fonte_csv)
-    etl_csv.executar_etl()
+        print("Dados transformados:", dados_transformados)
+        nome_arquivo = os.path.join(os.path.dirname(__file__), os.getenv('NOME_ARQUIVO_SQLITE'))
+        with sqlite3.connect(nome_arquivo) as conexao:
+            dados_transformados.to_sql("exemplo.db", conexao, if_exists='replace', index=False)
--- Pratico/05 - Getter_Setter/decorator.py
+++ Pratico/05 - Getter_Setter/decorator.py
@@ -6,30 +6,33 @@ class Pessoa:
     @property
     def nome(self):
         return self._nome
-
+    
     @nome.setter
     def nome(self, novo_nome):
         self._nome = novo_nome
-
+    
     @property
     def idade(self):
         return self._idade
-
+    
     @idade.setter
     def idade(self, nova_idade):
         if nova_idade > 0:
             self._idade = nova_idade
         else:
-            print("A idade deve ser um número positivo.")
+            print("Idade deve ser um número positivo.")
 
-# Exemplo de uso
-pessoa = Pessoa("João", 30)
+pessoa = Pessoa("Arthur", 24)
 
-# Usando o método getter para acessar o atributo 'nome'
-print("Nome:", pessoa.nome)  # Saída: Nome: João
+print("Nome: ", pessoa.nome)
 
-# Usando o método setter para alterar o atributo 'nome'
-pessoa.nome = "Maria"
-print("Novo nome:", pessoa.nome)  # Saída: Novo nome: Maria
+print("Idade: ", pessoa.idade)
 
-# Usando o método get
+pessoa.nome = "Duda"
+pessoa.idade = 22
+
+print("Nome: ", pessoa.nome)
+
+print("Idade: ", pessoa.idade)
+
+pessoa.idade = -1
